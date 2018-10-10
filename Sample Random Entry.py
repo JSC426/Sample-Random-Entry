6e84e3ec1e8a89ff1b0fcdaab9cc7a5f446c161d
JSC426 <42790948+JSC426@users.noreply.github.com>
2018-10-10 09:35:43 -0700

    Add files via upload

---
+++ Sample Random Entry.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def index_check(index_j, array_test):
+"""checks if next entry in list/array exists"""
+    try:
+        array_test[index_j]
+        status = True 
+    except:
+        status = False
+        
+    return status
+
+def random_sample(long_array):
+"""randomly samples single entry from a list/array while only storing one entry"""
+    stored_value = long_array[0]
+    count = 1
+    
+    while index_check(count, long_array):
+        if np.random.uniform(0, 1, 1) < (1/(count + 1)):
+            stored_value = long_array[count]
+        count += 1
+            
+    return stored_value
